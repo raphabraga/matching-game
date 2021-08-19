793131a2254f68970969d32e02f11289af45508a
raphabraga <raphael-braga@hotmail.com>
2021-08-19 18:10:57 -0300

    added the functionalities for clicking in two similar cards or different. added the functionalilty when the game is over. and make the order random

--- js/script.js
+++ js/script.js
@@ -1,7 +1,61 @@
 const cards = document.querySelectorAll(".card");
+let flippedTwoCards = false;
+let firstCard = null;
+let secondCard = null;
+let matchingPairs = 0;
+
+const gameOver = () => {
+  setTimeout(() => {
+    cards.forEach((card) => {
+      card.addEventListener("mouseup", flipCard);
+      card.classList.remove("flip");
+    });
+  }, 800);
+  setTimeout(() => {
+    shuffleCard();
+    alert("You win!");
+  }, 2000);
+};
 
 function flipCard() {
-  this.classList.toggle("flip");
+  if (firstCard == null) {
+    firstCard = this;
+    this.classList.add("flip");
+  } else {
+    if (secondCard == null && firstCard != this) {
+      secondCard = this;
+      flippedTwoCards = true;
+      this.classList.add("flip");
+    }
+  }
+  if (flippedTwoCards) {
+    flippedTwoCards = false;
+    if (firstCard.children[0].src === secondCard.children[0].src) {
+      firstCard.removeEventListener("mouseup", flipCard);
+      secondCard.removeEventListener("mouseup", flipCard);
+      firstCard = null;
+      secondCard = null;
+      matchingPairs++;
+      if (matchingPairs === 6) {
+        gameOver();
+      }
+    } else {
+      setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+        firstCard = null;
+        secondCard = null;
+      }, 1000);
+    }
+  }
 }
 
-cards.forEach((card) => card.addEventListener("click", flipCard));
+const shuffleCard = () => {
+  cards.forEach((card) => {
+    let randomPosition = Math.floor(Math.random() * 12);
+    card.style.order = randomPosition;
+  });
+};
+
+shuffleCard();
+cards.forEach((card) => card.addEventListener("mouseup", flipCard));
